daa6bdeb9498ab749f25c221a626e914d692876c
matheusfbonfim <matheusbonfim@alunos.utfpr.edu.br>
2020-12-11 00:16:56 -0300

    Update End #03

--- 03-async-await/src/main.js
+++ 03-async-await/src/main.js
@@ -17,24 +17,43 @@
 
 import axios from 'axios';
 
-async function getUserFromGithub(user) {
+// async function getUserFromGithub(user) {
+//   try{
+//     const response = await axios.get(`https://api.github.com/users/${user}`);
+//     console.log(response);
+//   }catch(err){
+//     console.log('Usuário não existe');
+//   }
+// }
+
+// getUserFromGithub('diego3g');
+// getUserFromGithub('diego3g124123');
+
+// ===============================================================
+
+// class Github {
+//   static async getRepositories(repo){
+//       try{
+//         const response = await axios.get(`https://api.github.com/repos/${repo}`);
+//         console.log(response.data);
+//       } catch (err){
+//         console.log("RESPOSITÓRIO não existe.");
+//       }
+//   }
+// }
+// Github.getRepositories('rocketseat/rocketseat.com.br');
+// Github.getRepositories('rocketseat/dslkvmskv');
+
+
+// ===============================================================
+
+const buscaUsuario = async (usuario) => {
   try{
-    const response = await axios.get(`https://api.github.com/users/${user}`);
-    console.log(response);
+    const response = await axios.get(`https://api.github.com/users/${usuario}`);
+    console.log(response.data);
   }catch(err){
     console.log('Usuário não existe');
   }
-}
-
-getUserFromGithub('diego3g');
-getUserFromGithub('diego3g124123');
-
-// ===============================================================
-
-// ===============================================================
-
-// ===============================================================
-
-// ===============================================================
-
+};
+ buscaUsuario('diego3g');
 // ===============================================================
